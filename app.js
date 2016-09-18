c89476e40ee834d344c68fdcf71659499a5507e9
Andrés Gaudioso <andres.gaudioso@outlook.es>
2016-09-18 13:28:34 +0200

    Actualizado el proyecto funcionalmente

--- app.js
+++ app.js
@@ -1,20 +1,41 @@
 (function () {
     'use strict';
-    angular.module('myFirstApp', []).
-        controller('myFirstController', function ($scope) {
-            $scope.name = "Andrés";
-            $scope.total = 0;
-            console.log("Empieza");
-            $scope.displayvalue = function () {
-                $scope.total = calculateValue($scope.name);
-            };
+    angular.module('LunchCheck', []).
+        controller('LunchCheckController', LunchCheckController);
 
-            function calculateValue(string) {
-                var totalStringValue = 0;
-                for (var i = 0; i < string.length; i++) {
-                    totalStringValue += string.charCodeAt(i);
-                }
-                return totalStringValue;
-            };
-        });
+    LunchCheckController.$inject = ['$scope'];
+
+    function LunchCheckController($scope) {
+        $scope.message = "Enjoy!";
+        $scope.items = "";
+        $scope.total = calculateItems($scope.items);
+        $scope.validateMessage="";
+        console.log("Empieza");
+
+        $scope.enoughtItems = function () {
+            $scope.total = calculateItems($scope.items);
+
+            if ($scope.total.length > 3) {
+                $scope.message = "Too Much";
+            } else {
+                $scope.message = "Enjoy!";
+            }
+        };
+
+        $scope.checkEnought = function () {
+            console.log("Realizada la llamada " + $scope.total.length);
+            if ($scope.items == '') {
+               $scope.validateMessage = "Please enter data first";
+            } else {
+               $scope.validateMessage = "";
+            }
+
+        };
+
+        function calculateItems(string) {
+            var totalStringValue = string.split(',');
+            console.log(totalStringValue.length);
+            return totalStringValue;
+        };
+    };
 })();
